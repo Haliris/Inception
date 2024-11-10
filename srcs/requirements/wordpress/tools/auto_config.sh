a149babc47e239cc29eaab703220affb247f6b66
jteissie <jteissie@inception.inception.jteissie.org>
2024-11-10 18:58:36 +0100

    Weird php erorr in wordpress dockerfile, but seems to install properly and tries to run?

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -3,10 +3,10 @@
 #Make sure MariaDB has started properly
 sleep 10
 
-if [ ! -d "/var/www/html/wordpress/wp-config.php" ]; then
+if [ ! -d "/var/www/wordpress/wp-config.php" ]; then
 	wp config create --allow-root \
 		--dbname=$SQL_DATABASE \
 		--dbuser=$SQL_USER \
 		--dbpass=$SQL_PASSWORD \
-		--dbhost=mariadb:3306 --path='/var/www/html/wordpress'
+		--dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
