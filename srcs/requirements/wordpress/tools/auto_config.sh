5b27bf516b239c8272da422f42f5cf37ef34c824
jteissie <jteissie@inception.inception.jteissie.org>
2024-11-14 15:39:50 +0100

    redis works???

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -19,11 +19,6 @@ else
 		--dbhost=mariadb:3306 \
 		--path='/var/www/html' || { echo "Wordpress core config failed"; exit 1; }
 
-	echo "Setting up redis-cache..."
-	wp config set WP_CACHE true --raw || { echo "Redis-cache CACHE config setup failed"; exit 1; }
-	wp config set WP_REDIS_HOST 'redis' || { echo "Redis-cache HOST config setup failed"; exit 1; }
-	wp config set WP_REDIS_PORT 6379 --raw || { echo "Redis-cache PORT config setup failed"; exit 1; }
-
 	echo "Installing WordPress..."
 	wp core install \
 		--url="${WP_URL}" \
@@ -33,6 +28,20 @@ else
 		--admin_email="${WP_ADMIN_EMAIL}" \
 		--allow-root || { echo "Wordpress core install failed"; exit 1; }
 
+
+	echo "Installing Redis Cache plugin..."
+	wp plugin install redis-cache --activate --allow-root || { echo "Failed to install redis-cache plugin"; exit 1; }
+	echo "Setting up redis-cache..."
+	wp config set WP_CACHE true --raw --allow-root|| { echo "Redis-cache CACHE config setup failed"; exit 1; }
+	wp config set WP_REDIS_HOST 'redis' --allow-root || { echo "Redis-cache HOST config setup failed"; exit 1; }
+	wp config set WP_DEBUG true --raw --allow-root || { echo "Redis-cache WP_DEBUG config setup failed"; exit 1; }
+	wp config set WP_REDIS_PORT 6379 --raw --allow-root || { echo "Redis-cache PORT config setup failed"; exit 1; }
+
+	echo "Installing php-redis extension"
+	apt-get install -y php-redis || { echo "Failed to install redis php extension"; exit 1; }
+	#service php7.4-fpm restart || { echo "Failed to restart php7.4-fpm"; exit 1; }
+	wp plugin activate redis-cache --allow-root
+	wp redis enable --allow-root || { echo "Could not enable object caching"; exit 1; }
 	echo "Creating user..."
 	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
 		--role=subscriber \
