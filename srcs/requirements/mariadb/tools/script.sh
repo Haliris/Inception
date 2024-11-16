6f0b8c78aa11e1c24322fbb21eda98cd638885d1
jteissie <jteissie@inception.inception.jteissie.org>
2024-11-16 16:16:55 +0100

    Containers build with elastic

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -12,7 +12,7 @@ sleep 3
 mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';"
 mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rootpassword';
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rootpassword';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
